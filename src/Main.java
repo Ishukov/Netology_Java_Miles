de08e8e825d2bccd7d96ae874421688b4b2fbd95
s_ishukov <s_ishukov@bk.ru>
2023-01-30 16:30:06 +0500

    Code reformat

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
         BonusMilesService service = new BonusMilesService();
         int ticketPrice = 10000;
         int miles = service.calculate(ticketPrice);
-
         System.out.println("Стоимость вашего билета " + ticketPrice + " руб.");
         System.out.println("Вам начисленно " + miles + " миль");
-
-
     }
-
 }
